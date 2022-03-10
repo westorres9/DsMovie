fb6a45d21eb3c2171869423dadd2bac3d1aee43e
westorres9 <wester.torres9@gmail.com>
2022-03-10 16:31:53 -0300

    frontend updated

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,6 +1,5 @@
 import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
 import { MoviePage } from 'types/movie';
-import { isPropertyAssignment } from 'typescript';
 import './styles.css';
 
 type Props = {
