0705d30b912c33a75c4606ad0205b73682c08cbe
westorres9 <wester.torres9@gmail.com>
2022-03-10 11:54:07 -0300

    frontend updated

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as GthubIcon} from 'assets/img/github.svg';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Navbar () {
@@ -7,7 +8,9 @@ function Navbar () {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <Link to="/">
+                        <h1>DSMovie</h1>
+                    </Link>
                     <a href="https://github.com/westorres9" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GthubIcon/>
